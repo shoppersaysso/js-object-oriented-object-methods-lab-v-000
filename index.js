6a5ec288d23daf042350596df00d5997d4b9dd2e
Sara Hopper <sara.hopper@gmail.com>
2017-09-28 02:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function BoardMember(name, homeState, training) {
   }
   this.approve = function(){
     return `You can do that!`;
-}
+  }
   this.doCharity = function(){
     return `I like to help people.`;
-}
+  }
   this.releasePressStatement = function(){
   return `You will see great things from Scuber.`;
-}
+  }
   this.sayHi = function(){
     return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.`;
-}
   }
+}
